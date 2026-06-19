398586bb4417274eb8c02a923eff0176e6b26b97
ryudaewan <ryudaewan@gmail.com>
2026-06-19 11:18:59 +0900

    EventGrid 생성 안됨에 따라 옛날 방식으로 돌아감

--- function_app.py
+++ function_app.py
@@ -3,6 +3,6 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="pptx", path="raw-proposal/{name}", connection="PptxRepoConnection", source="EventGrid")
+@app.blob_trigger(arg_name="pptx", path="raw-proposal/{name}", connection="PptxRepoConnection")
 def blob_trigger(pptx: func.InputStream):
     logging.info(f"Blob trigger: name={pptx.name}, size={pptx.length} bytes")
