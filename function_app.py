eb71eaeff094c3baf465bec5787879618b0d2ab9
ryudaewan <ryudaewan@gmail.com>
2026-06-19 15:26:16 +0900

    EventGrid 가즈아!

--- function_app.py
+++ function_app.py
@@ -3,6 +3,6 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="pptx", path="raw-proposal/{name}", connection="PptxRepoConnection")
+@app.blob_trigger(arg_name="pptx", path="raw-proposal/{name}", connection="PptxRepoConnection", source=func.BlobTriggerSource.EVENT_GRID)
 def blob_trigger(pptx: func.InputStream):
     logging.info(f"Blob trigger: name={pptx.name}, size={pptx.length} bytes")
