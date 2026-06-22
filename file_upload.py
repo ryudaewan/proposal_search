6249ea8d8fe88e22bd9422f99bd0d7320c340d4e
ryudaewan <ryudaewan@gmail.com>
2026-06-22 15:17:18 +0900

    불필요한 파일 제거

--- file_upload.py
+++
@@ -1,7 +0,0 @@
-from azure.storage.blob import BlobServiceClient
-
-conn = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OiduX+d0/dY=;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
-client = BlobServiceClient.from_connection_string(conn)
-
-with open("test.pptx", "rb") as f:
-    client.get_blob_client("raw-proposal", "test.pptx").upload_blob(f)
